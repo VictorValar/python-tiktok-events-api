41748378e681b627be3fb0c12c2535f4e23bbdc2
Victor Valar <valar@victorvalar.me>
2023-09-11 11:52:35 -0300

    Add validator to Properties and update docstrings

--- pytt_events/properties.py
+++ pytt_events/properties.py
@@ -1,15 +1,18 @@
 from pydantic import (
     BaseModel,
-    ValidationError,
     conlist,
     constr,
     PositiveFloat,
-    PositiveInt
+    PositiveInt,
+    root_validator
 )
 from typing import Optional
 from enum import Enum
+from utils import PropertiesFormatError
 
 class ContentType(str, Enum):
+    """ Content type enum."""
+
     """
     For individual products.
     """
@@ -22,22 +25,57 @@ class ContentType(str, Enum):
     def __str__(self):
         return self.value
 
+
 class Content(BaseModel):
-    content_id: constr(strip_whitespace=True, min_length=1)
+    """ A list of content objects that represent products.
+
+    Attributes:
+        content_id (str): [Required] Unique ID of the product or content.
+        quantity (int): [Required] The quantity of the content.
+        price (float): [Required] The price of the item.
+        content_category (str): [Optional] The category of the content.
+        content_name (str): [Optional] The name of the content.
+    """
+
+    content_id: Optional[constr(strip_whitespace=True, min_length=1)]
     quantity: PositiveInt
     price: PositiveFloat
-    content_category: constr(min_length=1)
-    content_name: constr(min_length=1)
+    content_category: Optional[constr(min_length=1)]
+    content_name: Optional[constr(min_length=1)]
+
 
 class Properties(BaseModel):
-    currency: Optional[constr(strip_whitespace=True, min_length=3, max_length=3)]
+    """ Properties associated with the event.
+
+    Attributes:
+        currency (str): [Optional] ISO 4217 currency code.
+        value (float): [Optional] The value of the event.
+        description (str): [Optional] The description of the event.
+        query (str): [Optional] The query of the event.
+        status (str): [Optional] The status of the event.
+        content_type (ContentType): [Optional] The content type of the event.
+        contents (list[Content]): [Optional] A list of content objects that represent products.
+    """
+    currency: Optional[constr(
+        strip_whitespace=True, min_length=3, max_length=3
+    )]
     value: Optional[PositiveFloat]
     description: Optional[constr(min_length=1)]
     query: Optional[constr(min_length=1)]
     status: Optional[constr(min_length=1)]
-    content_type: ContentType
+    content_type: Optional[ContentType]
     contents: Optional[conlist(
         item_type=Content, unique_items=True, min_items=1
     )]
+
+    @root_validator(pre=True)
+    @classmethod
+    def validate_user(cls, values):
+        if not any(values.values()):
+            raise PropertiesFormatError(
+                value=values,
+                message='Properties must have at least one attribute.'
+            )
+        return values
 
 
